0775c740e9cc177f10f9e3dd1ddebd66e0f58b59
jsdip <kaur5940@sheridancollege.ca>
2019-05-19 16:06:54 -0400

    new repo for students

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
+import java.beans.PropertyChangeSupport;
+
 /**
  *
  * @author mehta date
@@ -81,6 +83,11 @@ public class Student {
     public void setMaxCourse(int maxCourse) {
         this.maxCourse = maxCourse;
     }
+    private final transient PropertyChangeSupport propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
+    public static final String PROP_NAME = "name";
+    public static final String PROP_ID = "ID";
+    public static final String PROP_PROGRAM = "Program";
+    public static final String PROP_MAXCOURSE = "maxCourse";
     
    
     
